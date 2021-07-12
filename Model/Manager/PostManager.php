bc15bb2f01208cc4e4f04c20b17970c7aacc6615
Yoann Corsi <yoann.corsi@gmail.com>
2021-07-12 12:38:57 +0200

    modif

--- Model/Manager/PostManager.php
+++ Model/Manager/PostManager.php
@@ -1,26 +1,25 @@
 <?php
-	class PostManager extends BaseManager
+class PostManager extends BaseManager
+{
+	public function __construct($datasource)
 	{
-		public function __construct($datasource)
-		{
-			parent::__construct("article","Post",$datasource);	
-		}
-		public function create($title, $content)
+		parent::__construct("article", "Post", $datasource);
+	}
+	public function create($title, $content)
 	{
 		$req = $this->bdd->prepare('INSERT INTO article(title,content,date) VALUES(?, ?, NOW())');
-		$affectedLines = $req->execute(array( $title, $content));
-		
+		$affectedLines = $req->execute(array($title, $content));
+
 
 		return $affectedLines;
 	}
 	public function signal($id)
 	{
 		$req = $this->bdd->prepare('UPDATE comment SET signaled = 1 WHERE comment.id = ?');
-	$affectedLines = $req->execute(array($id));
-		
-		
+		$affectedLines = $req->execute(array($id));
+
+
 		return $affectedLines;
 	}
-	
-     }
-        
+
+}
